feb3dbfb5b12a308911638578f0335e4b7fc863e
adriennefranke <18449966+adriennefranke@users.noreply.github.com>
2020-06-24 20:42:40 -0500

    Create basic functionality for the CLI for searching the NPPES API

--- nppes/search.py
+++ nppes/search.py
@@ -1,22 +1,42 @@
 import argparse
-import requests 
+import requests
+import pandas as pd
 
 # create parser
-parser = argparse.ArgumentParser(description='Search the NPPES.')
+nppes_parser = argparse.ArgumentParser(description='search the NPPES file from your terminal')
 
 # add optional arguments to the parser
-parser.add_argument('--npi', help="a healthcare provider's National Provider Identifier")
-parser.add_argument('--enumeration_type', help="1: people, 2: places")
-parser.add_argument('--taxonomy', help='exact description or exact specialty')
-parser.add_argument('--first_name', help="a healthcare provider's first name")
-parser.add_argument('--last_name', help="a healthcare provider's last name")
-parser.add_argument('--organization_name', help="a healthcare organization's name")
-parser.add_argument('--address_purpose', help='options: location, mailing, primary, secondary')
-parser.add_argument('--city', help='the city a healthcare provider is located in')
-parser.add_argument('--postal_code', help='the zip code a healthcare provider is located in')
-parser.add_argument('--limit', help='limit results, default is 10 and max is 200')
+nppes_parser.add_argument('--number', help="a healthcare provider's National Provider Identifier")
+nppes_parser.add_argument('--enumeration_type', choices=range(1,2), help="1: people, 2: places")
+nppes_parser.add_argument('--taxonomy_description', help='exact description or exact specialty')
+nppes_parser.add_argument('--first_name', help="a healthcare provider's first name")
+nppes_parser.add_argument('--last_name', help="a healthcare provider's last name")
+nppes_parser.add_argument('--organization_name', help="a healthcare organization's name")
+nppes_parser.add_argument('--address_purpose', help='options: location, mailing, primary, secondary')
+nppes_parser.add_argument('--city', help='the city a healthcare provider is located in')
+nppes_parser.add_argument('--postal_code', help='the zip code a healthcare provider is located in')
+nppes_parser.add_argument('--limit', help='limit results, default is 10 and max is 200')
 
 # parse args into a dict
-args = parser.parse_args()
-args_dict = vars(args)
+nppes_args = nppes_parser.parse_args()
+nppes_args_dict = vars(nppes_args)
 
+# create search function
+def search_nppes_api(args: dict) -> str:
+    '''
+    Uses arguments from the NPPES CLI to search the NPPES API
+
+    :param args: a dictionary of arguments from the CLI
+    '''
+    print('getting data from NPPES API...')
+    
+    nppes_api_endpoint = 'https://npiregistry.cms.hhs.gov/api/?version=2.1'
+    r = requests.get(nppes_api_endpoint, params=args)
+    
+    nppes_result_json = r.json()
+    nppes_result_df = pd.json_normalize(nppes_result_json['results'])
+    
+    print(nppes_result_json)
+
+# run search_nppes_api function
+search_nppes_api(nppes_args_dict)
