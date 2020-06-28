2f967240ffdc37115bca56854ad439d75f496050
adriennefranke <18449966+adriennefranke@users.noreply.github.com>
2020-06-28 17:15:31 -0500

    Create simple, v1 tests

--- tests/test_nppes.py
+++ tests/test_nppes.py
@@ -1,5 +1,16 @@
 from nppes import __version__
+import click.testing
+import pytest
+from nppes.search import search_nppes_api
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+@pytest.fixture
+def runner():
+    return click.testing.CliRunner()
+
+def test_search_nppes_api(runner):
+    result = runner.invoke(search_nppes_api, ['--last_name', 'Gomez'])
+    assert result.exit_code == 0
