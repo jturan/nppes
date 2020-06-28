038a1257077fe12e4df39195d46ce718a1e52498
adriennefranke <18449966+adriennefranke@users.noreply.github.com>
2020-06-28 17:07:24 -0500

    Switch over to using Click rather than Argparse for the CLI

--- nppes/search.py
+++ nppes/search.py
@@ -1,43 +1,45 @@
-import argparse
 import requests
-import pandas as pd
+import click
+import json
+from pygments import highlight, lexers, formatters
 
-# create parser
-nppes_parser = argparse.ArgumentParser(description='search the NPPES file from your terminal')
+def format_json_response(json_data):
+    formatted_json = json.dumps(json_data, sort_keys=True, indent=4)
+    highlighted_json = highlight(formatted_json, lexers.JsonLexer(), formatters.TerminalFormatter())
+    return highlighted_json
 
-# add optional arguments to the parser
-nppes_parser.add_argument('--number', help="a healthcare provider's National Provider Identifier")
-nppes_parser.add_argument('--enumeration_type', choices=range(1,2), help="1: people, 2: places")
-nppes_parser.add_argument('--taxonomy_description', help='exact description or exact specialty')
-nppes_parser.add_argument('--first_name', help="a healthcare provider's first name")
-nppes_parser.add_argument('--last_name', help="a healthcare provider's last name")
-nppes_parser.add_argument('--organization_name', help="a healthcare organization's name")
-nppes_parser.add_argument('--address_purpose', help='options: location, mailing, primary, secondary')
-nppes_parser.add_argument('--city', help='the city a healthcare provider is located in')
-nppes_parser.add_argument('--state', help='the state a healthcare provider is located in')
-nppes_parser.add_argument('--postal_code', help='the zip code a healthcare provider is located in')
-nppes_parser.add_argument('--limit', help='limit results, default is 10 and max is 200')
+@click.command()
+@click.option('--number', help="a healthcare provider's National Provider Identifier")
+@click.option('--enumeration_type', help="1: people, 2: places")
+@click.option('--taxonomy_description', help='exact description or exact specialty')
+@click.option('--first_name', help="a healthcare provider's first name")
+@click.option('--last_name', help="a healthcare provider's last name")
+@click.option('--organization_name', help="a healthcare organization's name")
+@click.option('--address_purpose', help='options: location, mailing, primary, secondary')
+@click.option('--city', help='the city a healthcare provider is located in')
+@click.option('--state', help='the state a healthcare provider is located in')
+@click.option('--postal_code', help='the zip code a healthcare provider is located in')
+@click.option('--limit', help='limit results, default is 10 and max is 200')
+def search_nppes_api(number, enumeration_type, taxonomy_description, first_name, last_name, organization_name, address_purpose, city, state, postal_code, limit):
+    click.echo(click.style('searching the NPPES API...🔦', fg='blue'))
+    nppes_api_url = 'https://npiregistry.cms.hhs.gov/api/?version=2.1'
+    query_params = {
+        'number': number,
+        'enumeration_type': enumeration_type,
+        'taxonomy_description': taxonomy_description,
+        'first_name': first_name,
+        'last_name': last_name,
+        'organization_name': organization_name,
+        'address_purpose': address_purpose,
+        'city': city,
+        'state': state,
+        'postal_code': postal_code,
+        'limit': limit 
+    }
+    r = requests.get(nppes_api_url, params=query_params)
+    json_data = r.json()
+    formatted_json_data = format_json_response(json_data)
+    click.echo(formatted_json_data)
 
-# parse args into a dict
-nppes_args = nppes_parser.parse_args()
-nppes_args_dict = vars(nppes_args)
-
-# create search function
-def search_nppes_api(args: dict) -> dict:
-    '''
-    Uses arguments from the NPPES CLI to search the NPPES API
-
-    :param args: a dictionary of arguments from the CLI
-    '''
-    print('getting data from NPPES API...')
-    
-    nppes_api_endpoint = 'https://npiregistry.cms.hhs.gov/api/?version=2.1'
-    r = requests.get(nppes_api_endpoint, params=args)
-    
-    nppes_result_json = r.json()
-    nppes_result_df = pd.json_normalize(nppes_result_json['results'])
-    
-    print(nppes_result_json)
-
-# run search_nppes_api function
-search_nppes_api(nppes_args_dict)
+if __name__ == '__main__':
+    search_nppes_api()
