af97ca4baab6900b1a437f76e66652cc05ae69d1
Jason Turan <jason.turan@gmail.com>
2020-07-04 16:22:43 -0500

    Add basic download functions

--- nppes/__init__.py
+++ nppes/__init__.py
@@ -1,3 +1,1 @@
 __version__ = '0.1.0'
-
-from .files import extract, transform, load, etl
--- nppes/files.py
+++ nppes/files.py
@@ -1,14 +1,53 @@
-def extract():
-    print("Download file")
+from zipfile import ZipFile
+
+import requests
+
+import settings
 
 
-def transform():
-    print("Transform file")
+BASE_URL = 'https://download.cms.gov/nppes/'
+BASE_FILE_NAME = 'NPPES_Data_Dissemination_'
 
 
-def load():
-    print("Load file")
+def validate_url():
+    r = requests.head(url)
+    if r.status_code == 200:
+        return True
+    else:
+        return False
 
 
-def etl():
-    print("Run full ETL pipeline on file")
+def download_file(url, save_path, chunk_size=128):
+    r = requests.get(url, stream=True)
+    with open(save_path, 'wb') as fd:
+        print("Downloading...")
+        for chunk in r.iter_content(chunk_size=chunk_size):
+            fd.write(chunk)
+        print("Done!")
+
+
+def unzip_file(file_name):
+    with ZipFile(file_name, 'r') as z:
+        z.printdir()
+        
+        print("Unzipping...")
+        z.extractall()
+        print("Done!")
+
+
+def extract(url, unzip=False):
+    save_path = f'{settings.DOWNLOADS_FOLDER}{file_name}'
+    
+    # validate url
+    is_valid_url = validate_url(url)
+    
+    # download file
+    if is_valid_url:
+        download_file(url, save_path)
+    else:
+        print("Invalid URL")
+        return
+    
+    # unzip file
+    if unzip:
+        unzip_file(save_path)
