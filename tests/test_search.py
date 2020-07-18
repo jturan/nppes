87474a6a6bebfb610f39f2057a0690264991e871
adriennefranke <18449966+adriennefranke@users.noreply.github.com>
2020-07-17 23:26:22 -0500

    Add test for search_nppes_api function

--- tests/test_search.py
+++ tests/test_search.py
@@ -11,6 +11,16 @@ def test_version():
 def runner():
     return click.testing.CliRunner()
 
-def test_search_nppes_api(runner):
-    result = runner.invoke(search_nppes_api, ['--last_name', 'Gomez'])
+def test_search_nppes_api(runner, mock_requests_get):
+    result = runner.invoke(search_nppes_api)
     assert result.exit_code == 0
+
+@pytest.fixture
+def mock_requests_get(mocker):
+    mock = mocker.patch("requests.get")
+    mock.return_value.json.return_value = {
+        "id": 1,
+        "name": "Stephen King",
+        "username": "kingofhorror"
+    }
+    return mock
