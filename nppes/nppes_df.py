ad1b05e14f70fa5cf4eb62823acfa0e64add05a2
adriennefranke <18449966+adriennefranke@users.noreply.github.com>
2020-08-17 22:04:38 -0500

    Create test file for nppes_df function

---
+++ nppes/nppes_df.py
@@ -0,0 +1,37 @@
+import requests
+import pandas as pd
+import numpy as np
+
+def nppes_df(**kwargs) -> pd.DataFrame:
+    '''
+    Searches the NPPES API based on various fields and returns a DataFrame with the results
+    
+    :param number: a healthcare provider's National Provider Identifier
+    :param enumeration_type: the type of healthcare provider, 1: people, 2: places
+    :param taxonomy_description: exact description or exact specialty
+    :param first_name: a healthcare provider's first name
+    :param last_name: a healthcare provider's last name
+    :param organization_name: a healthcare organization's name
+    :param address_purpose: the type of address (location, mailing, primary, or specialty)
+    :param city: the city a healthcare provider is located in
+    :param state: the state a healthcare provider is located in
+    :param postal_code: the zip code a healthcare provider is located in
+    :param limit: limit results, default is 10 and max is 200
+    '''
+    search_params = locals()['kwargs']
+    
+    user_defined_search_params = list(search_params.keys())
+    allowed_search_params = ['number', 'enumeration_type', 'taxonomy_description', 'first_name', 'last_name', 'organization_name', 'address_purpose', 'city', 'state', 'postal_code', 'limit']
+    disallowed_search_params = np.setdiff1d(user_defined_search_params, allowed_search_params)
+    
+    if len(disallowed_search_params) > 0:
+        print('Ensure your search parameters are valid. Invalid search params: ', disallowed_search_params)
+
+    else:
+        print('Searching the NPPES API...🔦')
+        nppes_api_url = 'https://npiregistry.cms.hhs.gov/api/?version=2.1'
+        json_data = requests.get(nppes_api_url, params=search_params).json()
+        return pd.json_normalize(json_data['results'])
+
+
+nppes_df(first_name='julia', last_name='gomez')
---
+++ tests/test_nppes_df.py
@@ -0,0 +1,5 @@
+from nppes import __version__
+from nppes.nppes_df import nppes_df
+
+def test_nppes_df():
+    print('Test not yet written.')
